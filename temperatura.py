acb421489918e1ed9dbea99dd5ed3c6bf309d5e3
JadroMontes <89556247+JadroMontes@users.noreply.github.com>
2021-11-06 00:57:34 -0700

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+while True:
+# Se declaran las variables de los tres sensores
+  
+  t = sense.get_temperature() 
+  p = sense.get_pressure() 
+  h = sense.get_humidity() 
+  # Se redondean los valores a decimal
+  t = round(t, 1)
+  p = round(p, 1)
+  h = round(h, 1)
+  # Creamos el metodo mensaje, usando  str() que va a mostrar la cadena que se mostrara en los led´s
+  message = "Temperatura: " + str(t) + " Presión: " + str(p) + " Humedad: " + str(h)
+  # Se muestra el metodo
+  sense.show_message(message)
